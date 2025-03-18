9e2006824e69ef2075d06d568ee34ddbea57438c
Mourad Ouammou <MouradOuammou8@gmail.com>
2025-03-18 15:39:10 +0000

    avancement 2

--- src/main/java/tp3/c/web/PatientController.java
+++ src/main/java/tp3/c/web/PatientController.java
@@ -1,23 +1,31 @@
 package tp3.c.web;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import tp3.c.entites.*;
 import tp3.c.repository.PatientRepository;
-
-import java.util.List;
 
 @Controller
 @AllArgsConstructor
 public class PatientController {
-    private PatientRepository patientRepository;
-    @GetMapping("/index")
-        public String index(Model model) {
-        List<Patient> Listpatients = patientRepository.findAll();
-        model.addAttribute("patients", Listpatients);
-        return "patient";
-        }
 
+    private final PatientRepository patientRepository;
+
+    @GetMapping("/index")
+    public String index(Model model,
+                        @RequestParam(defaultValue = "0") int page,
+                        @RequestParam(defaultValue = "10") int pageSize) {
+        Page<Patient> patientPage = patientRepository.findAll(PageRequest.of(page, pageSize));
+        model.addAttribute("patients", patientPage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", patientPage.getTotalPages());
+        model.addAttribute("totalPatients", patientPage.getTotalElements());
+        return "patients";
+    }
 }
